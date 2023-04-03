ae2fe5f1f7b89cd942bc4847f83af520c381f69d
Rico <mailvonrico@gmail.com>
2023-04-03 21:50:29 +0200

    build mit Fetch API

--- src/app/services/form.service.ts
+++ src/app/services/form.service.ts
@@ -71,7 +71,8 @@ export class FormService {
         })
         console.log(grundbuchamtForm.get('name')?.value + ' wurde ermittelt.')
       }catch(err){
-        console.error('Das Amtsgericht konnte nicht ermittelt werden.')
+        console.error('Das Amtsgericht konnte nicht ermittelt werden.');
+        console.error(err);
       }
     }
   }
