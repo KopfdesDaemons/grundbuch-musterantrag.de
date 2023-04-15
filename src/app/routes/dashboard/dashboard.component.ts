ac0f3e9e92cec537d7a6b24a7ce8351d0defe713
Rico <mailvonrico@gmail.com>
2023-04-16 01:25:32 +0200

    btn all unset; dashboard table; gitignore uploads

--- src/app/routes/dashboard/dashboard.component.ts
+++ src/app/routes/dashboard/dashboard.component.ts
@@ -1,7 +1,7 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Component, ElementRef, HostListener, OnInit } from '@angular/core';
 import { lastValueFrom } from 'rxjs';
-import { faRotateRight, faCircleExclamation, faFileArrowDown } from '@fortawesome/free-solid-svg-icons';
+import { faRotateRight, faCircleExclamation, faFileArrowDown, faArrowUpRightFromSquare, faEllipsisVertical } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
@@ -11,6 +11,8 @@ export class DashboardComponent implements OnInit {
   faRotateRight = faRotateRight;
   faCircleExclamation = faCircleExclamation;
   faFileArrowDown = faFileArrowDown;
+  faArrowUpRightFromSquare = faArrowUpRightFromSquare;
+  faEllipsisVertical = faEllipsisVertical;
   infoJson: any;
   files: any[] | undefined;
   page: number = 0;
@@ -31,16 +33,18 @@ export class DashboardComponent implements OnInit {
     return new Promise(async (resolve, reject) => {
       try {
         console.log('Lade Daten...');
-        this.page++;
         if (this.infoJson && this.page > this.infoJson['totalPages']) return;
         if (!this.files) this.files = [];
         const url = '/api/uploads';
         const json: any = await lastValueFrom(this.http.get(url, { params: new HttpParams().set('page', this.page) }));
         this.infoJson = json;
-
+        console.log(this.infoJson);
+        
+        
         for (const file of json['files']) {
           this.files.push(file);
         }
+        this.page++;
       } catch (err) {
         console.error('Die Dateien konnten nicht geladen werden.');
         console.error(err);
@@ -59,16 +63,21 @@ export class DashboardComponent implements OnInit {
   onDocumentClick(event: any) {
     const dropDowns = this.elem.nativeElement.querySelectorAll('.dropDown');
     
-    for(const dropdown of dropDowns){
-      if(dropdown === event.target) continue;
+    for (const dropdown of dropDowns) {
       const ul = dropdown.querySelector('ul');
-      ul.style.visibility = 'collapse'
+      
+      if (ul.style.visibility === 'visible') {
+        if (!dropdown.contains(event.target)) {
+          ul.style.visibility = 'collapse';
+        }
+      }
     }
   }
 
   click(element: any){
-    const dropdown = element as HTMLElement;
-    const ulElement = dropdown.querySelector('ul');
+    const dropdown = element.closest('.dropDown') as HTMLElement;
+    const ulElement = dropdown.querySelector('.dropDownMenu') as HTMLElement;
+    
     if(ulElement) ulElement.style.visibility = 'visible';
   }
 
