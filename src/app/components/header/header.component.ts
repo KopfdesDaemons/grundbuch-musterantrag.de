f48dc51417f3b521aed282eb8f1202eee063c09e
Rico <mailvonrico@gmail.com>
2023-04-16 20:32:41 +0200

    fix header farbauswahl hover

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -13,6 +13,8 @@ import { DesignloaderService } from 'src/app/services/designloader.service';
 export class HeaderComponent implements AfterViewInit {
   faMoon = faMoon;
 
+  colors = [  'hsl(195, 75%, 50%)',  'hsl(0, 60%, 50%)',  'hsl(323, 82%, 50%)',  'hsl(132, 64%, 50%)',  'hsl(35, 100%, 50%)',  'hsl(173, 63%, 50%)',  'hsl(281, 94%, 50%)',  'hsl(81, 56%, 50%)',  'hsl(0, 0%, 50%)',  'hsl(334, 100%, 50%)',  'hsl(225, 6%, 50%)',  'hsl(110, 69%, 50%)'];
+
   @ViewChild('mobilenav') mobilenav!: ElementRef;
   @ViewChild('closingdiv') closingdiv!: ElementRef;
   @ViewChild('cookiebanner') cookiebanner!: ElementRef;
