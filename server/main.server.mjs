b3f3da64f98d9fedc036ceaecfd828393bcd6253
Rico <mailvonrico@gmail.com>
2024-07-13 22:23:45 +0200

    new home page: antragsliste

--- server/main.server.mjs
+++ server/main.server.mjs
@@ -1,10 +1,2 @@
 import './polyfills.server.mjs';
-import {
-  AppServerModule
-} from "./chunk-SZMOGDW6.mjs";
-import "./chunk-4VR5P7ZM.mjs";
-import "./chunk-YBYOXPTQ.mjs";
-export {
-  AppServerModule as default
-};
-//# sourceMappingURL=main.server.mjs.map
+import{a}from"./chunk-SXHUY6BI.mjs";import"./chunk-JDO7VFRF.mjs";import"./chunk-HBSEUJEB.mjs";export{a as default};
