b6c8ebc7b62bd9e70187381c77965189b602b9d6
Rico <mailvonrico@gmail.com>
2024-07-19 01:14:21 +0200

    fix for antragsinfos/grundbuchausdruck

--- server/main.server.mjs
+++ server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-P3OKY7AF.mjs";import"./chunk-JDO7VFRF.mjs";import"./chunk-HBSEUJEB.mjs";export{a as default};
+import{a}from"./chunk-Q4DT5PO6.mjs";import"./chunk-JDO7VFRF.mjs";import"./chunk-HBSEUJEB.mjs";export{a as default};
