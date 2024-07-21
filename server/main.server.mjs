64f50a7e7ca9a5ba10a484578c21d0fe6dc4a475
Rico <mailvonrico@gmail.com>
2024-07-21 22:17:15 +0200

    Antrag auf Grundbuchberichtigung aufgrund eines Sterbefalls

--- server/main.server.mjs
+++ server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-Q4DT5PO6.mjs";import"./chunk-JDO7VFRF.mjs";import"./chunk-HBSEUJEB.mjs";export{a as default};
+import{a}from"./chunk-KYWNPXMS.mjs";import"./chunk-JDO7VFRF.mjs";import"./chunk-HBSEUJEB.mjs";export{a as default};
