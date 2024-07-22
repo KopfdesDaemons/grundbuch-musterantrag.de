ab759f970222aaf4727cc83bc79aa42aa645a257
Rico <mailvonrico@gmail.com>
2024-07-22 22:03:27 +0200

    progressbar, more text content for antragsinfos/grundbuchausdruck

--- server/main.server.mjs
+++ server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-KYWNPXMS.mjs";import"./chunk-JDO7VFRF.mjs";import"./chunk-HBSEUJEB.mjs";export{a as default};
+import{a}from"./chunk-RN2224GC.mjs";import"./chunk-JDO7VFRF.mjs";import"./chunk-HBSEUJEB.mjs";export{a as default};
