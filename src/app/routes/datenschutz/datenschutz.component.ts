81a76b3bf21f295f3499c9b4dbd26fa0dbeebabf
Rico <mailvonrico@gmail.com>
2024-08-24 23:49:48 +0200

    dashboard: responsive grid, cookie list in datenschutzerklärung

--- src/app/routes/datenschutz/datenschutz.component.ts
+++ src/app/routes/datenschutz/datenschutz.component.ts
@@ -3,13 +3,14 @@ import { Title } from '@angular/platform-browser';
 import { CookiesService } from 'src/app/services/cookies.service';
 import { FooterComponent } from '../../components/footer/footer.component';
 import { HeaderComponent } from '../../components/header/header.component';
+import { CookiesTileComponent } from "../../components/dashboard/tiles/cookies-tile/cookies-tile.component";
 
 @Component({
-    selector: 'app-datenschutz',
-    templateUrl: './datenschutz.component.html',
-    styleUrls: ['./datenschutz.component.scss'],
-    standalone: true,
-    imports: [HeaderComponent, FooterComponent]
+  selector: 'app-datenschutz',
+  templateUrl: './datenschutz.component.html',
+  styleUrls: ['./datenschutz.component.scss'],
+  standalone: true,
+  imports: [HeaderComponent, FooterComponent, CookiesTileComponent]
 })
 export class DatenschutzComponent {
 
@@ -17,7 +18,7 @@ export class DatenschutzComponent {
     this.titleService.setTitle('Datenschutzerklärung');
   }
 
-  deleteCookies(){
+  deleteCookies() {
     this.cs.deleteAllCookies();
     alert('Allle Cookies gelöscht.');
   }
