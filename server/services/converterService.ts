8ca385858782cc6a13f74f0cf16d0c785ce7834d
Rico <mailvonrico@gmail.com>
2025-01-12 18:59:36 +0100

    github actions: cleanup docker files, openoffice --headless

--- server/services/converterService.ts
+++ server/services/converterService.ts
@@ -1,7 +1,7 @@
 import { spawn } from 'child_process';
 
 export const convertToPdf = async (docxFilePath: string, folderPath: string): Promise<void> => {
-    const args = ['--convert-to', 'pdf', docxFilePath, '--outdir', folderPath];
+    const args = ['--headless', '--convert-to', 'pdf', docxFilePath, '--outdir', folderPath];
 
     const child = spawn('soffice', args);
 
