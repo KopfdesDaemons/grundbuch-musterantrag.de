34ffdea71ecf787199b0c22b7285400f53df939a
Rico <mailvonrico@gmail.com>
2025-02-01 18:34:14 +0100

    config: remove logger

--- server/config/config.ts
+++ server/config/config.ts
@@ -1,24 +1,20 @@
 import path from 'path';
 import { fileURLToPath } from 'url';
-import logger from './logger';
 
 // environment variables
 export const JWT_SECRET: string = process.env['JWT_SECRET'] || '';
 if (!JWT_SECRET) {
     console.warn('⚠️ JWT_SECRET ist nicht definiert');
-    logger.info('JWT_SECRET ist nicht definiert');
 }
 
 export const DASHBOARD_ROOT_PASSWORD: string = process.env['DASHBOARD_ROOT_PASSWORD'] || '';
 if (!DASHBOARD_ROOT_PASSWORD) {
     console.warn('⚠️ DASHBOARD_ROOT_PASSWORD ist nicht definiert');
-    logger.info('DASHBOARD_ROOT_PASSWORD ist nicht definiert');
 }
 
 export const DASHBOARD_ROOT_USER: string = process.env['DASHBOARD_ROOT_USER'] || '';
 if (!DASHBOARD_ROOT_USER) {
     console.warn('⚠️ DASHBOARD_ROOT_USER ist nicht definiert');
-    logger.info('DASHBOARD_ROOT_USER ist nicht definiert');
 }
 
 
